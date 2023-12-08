b35f9fc5cb9b295a08b5f576c4d9dc06eb36d54c
Kamauleonard <leonardmwangi2012@gmail.com>
2023-12-08 13:08:46 +0300

    First Commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [word + '!' for word in sentence_list]
+
+print(return_evens([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+result = make_exclamation(["hi,", "my", "name", "sound", "john"])
+print(' '.join(result))
+ 
